03130939ef9ae2c93abb7dbc08c991513ccbafc8
Rodolfo Della Violla <rodolfo.violla@gmail.com>
2020-06-13 14:46:41 -0300

    Add Heroku Vars

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
 
+// Para uso em desenvolvimento e no Heroku
 const porta = process.env.PORT || 8080
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/linkapi'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 mongoose.connect(
-  'mongodb://localhost:27017/linkapi', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
+  mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }
 )
 
 requireDir('./src/models')
